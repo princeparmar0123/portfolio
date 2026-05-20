115e2d96e7141a1020bd58ef9d506b44d8ccec70
Prince Parmar <prince.parmar@biztoso.com>
2026-05-20 12:24:56 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-B0Wg_oAo.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/princeparmar/Desktop/Prince/react/prince_landing_page/prince_landing_page/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-MZMkeJrl.js"] }, "/": { filePath: "/Users/princeparmar/Desktop/Prince/react/prince_landing_page/prince_landing_page/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CuEvR_QS.js"] } }, clientEntry: "/assets/index-MZMkeJrl.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-C1Sx9Ok6.js";
+import "node:events";
+export {
+  w as default
+};
